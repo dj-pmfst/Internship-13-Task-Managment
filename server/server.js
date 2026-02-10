4dd53094bd4c4a6b363a07dce543b0a9488dd2e5
nduje <dujerancic@gmail.com>
2026-02-10 22:51:50 +0100

    Add tasks table and database initialization

--- server/server.js
+++ server/server.js
@@ -1,1 +1,27 @@
-console.log("Server is running...");
+import "dotenv/config";
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import { initDb } from "./db/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+const rootDir = path.resolve(__dirname, "");
+
+app.use(express.json());
+app.use(express.static(rootDir));
+
+const startServer = async () => {
+    await initDb();
+    app.listen(port, () => {
+        console.log(`Connected to DB, server listening on port ${port}`);
+    });
+}
+
+startServer().catch((error) => {
+  console.error("Failed to start server", error);
+  process.exit(1);
+});
